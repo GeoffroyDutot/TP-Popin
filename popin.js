888412feb48a41fdde6da06c11313781c977a3b5
Nicolas HAMY <45590404+NicolasHAMY@users.noreply.github.com>
2019-01-17 15:43:21 +0100

    Update popin.js

--- popin.js
+++ popin.js
@@ -1,8 +1,11 @@
 
-
 // recupérer la div Popin grâce à l'id
 var popin = document.getElementById('id01');
 
+var ouvreur = document.getElementById('ouvreur');
+var fermeur = document.getElementById('fermeur');
+
+var bouton_send = document.getElementById("send")
 // Affichage du popin en 1 click sur le boutton "Ouvrir"
 window.onclick = function(event) {
     if (event.target == popin) {
@@ -10,28 +13,25 @@ window.onclick = function(event) {
     }
 }
 
-// Commentaire validaton inscription
 
-        document.querySelector("form").addEventListener("submit", function(event){
-        event.preventDefault()
-        popin.style.display = "none";
-        var span = document.querySelector('span');
-        span.innerText='Vous êtes bien enregistré';
-  });
- 
-// Fermeture du Popin grâce au bouton "Echap"
-    window.onkeydown = function(event){
-    if (event.keyCode == 27) {
-        popin.style.display = "none";
-    }
+document.getElementById('saisie_mail').onkeydown = verifMail
+
+
+fermeur.onclick = function(){
+document.getElementById('id01').style.display='none'
 }
 
-// Verification champ E-mail
-function verifMail(champ)
-{ 
-   var regex = /^[a-zA-Z0-9.-]+@[a-z0-9.-]{2,}.[a-z]{1,4}$/;
+ouvreur.onclick = function(){
+    document.getElementById('id01').style.display='block'
+}
+
+
+function verifMail()
+    { champ = document.getElementById('saisie_mail').value
+   
+   var regex = /^[a-zA-Z0-9.-]+@[a-z0-9.-]{2,}.[a-z]{2,4}$/;
    var bouton_send = document.getElementById("send")
-   if(!regex.test(champ.value))
+   if(!regex.test(champ))
    {
     bouton_send.disabled = true;
     bouton_send.setAttribute("class", "disabled_button");
@@ -43,3 +43,49 @@ function verifMail(champ)
    }
 }
 
+//  traitement Ajax -> Script Php
+
+    document.querySelector("form").addEventListener("submit", function(e){
+
+    $.ajax({
+        url : 'script.php', // La ressource ciblée
+        type : 'POST',
+        data : 'email=' + document.getElementById('saisie_mail').value,
+        dataType: 'text',
+        success : function(erreur_php, ){
+            var span = document.querySelector("span");
+           
+            if(erreur_php=="OK"){
+                span.innerHTML = "Les données ont bien été enregistrées";
+            }
+            else{
+                span.innerHTML = "Il y'a eu une erreur dans le traitement pour la base de donnée (PHP)";
+            }
+        
+        },
+        error : function(){
+            var span = document.querySelector("span");
+            span.innerHTML = "Il y'a eu une erreur dans le traitement(AJAX)";
+            
+        },
+        
+        });
+
+
+    e.preventDefault();
+  });
+
+ 
+
+
+  window.onkeydown = function(event){
+    if (event.keyCode == 27) {
+        popin.style.display = "none";
+    }
+}
+
+
+document.querySelector("form").addEventListener("submit", function(e){
+        e.preventDefault();
+        popin.style.display = "none";
+});
